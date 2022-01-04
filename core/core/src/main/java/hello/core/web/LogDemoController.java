bf85dc8190af6d3dd92315dd7303493cf4d4de40
jintae_park <dktkd7044@gmail.com>
2022-01-05 03:41:27 +0900

    proxy 기술을 활용한 가짜객체 주입 실습 spring 기본편 마무리

--- core/core/src/main/java/hello/core/web/LogDemoController.java
+++ core/core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,16 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger logger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
-        myLogger.setRequestURL(requestURL);
+        logger.setRequestURL(requestURL);
 
-        myLogger.log("controller test");
+        logger.log("controller test");
         logDemoService.logic("Test Id");
         return "OK";
     }
