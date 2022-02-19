e5d1026ef495c26cf2f995ffc399cf78b63538ab
jintae_park <dktkd7044@gmail.com>
2022-02-19 14:44:02 +0900

    의존관계 주입은 생성자 주입을 사용하라

---
+++ spring-core/src/test/java/com/hello/springcore/order/OrderServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.hello.springcore.order;
+
+import com.hello.springcore.discount.FixDiscountPolicy;
+import com.hello.springcore.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L,"name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscount()).isEqualTo(1000);
+    }
+}
